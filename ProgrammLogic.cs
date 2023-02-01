baed0b67b9416f0ad86816261d61bb581d37b0a2
Vladimir Kidyaev <dreik2146@gmail.com>
2023-02-01 20:01:51 +0700

    feat: add new class `ProgramLogic`

---
+++ ProgrammLogic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiathlonProject
+{
+    class ProgrammLogic
+    {
+        private ConsoleInterface consoleInterface = new ConsoleInterface();
+        private BiathlonScheduleData data = new BiathlonScheduleData();
+
+        public void InitiateProgramm()
+        {
+            string answer = consoleInterface.AskUserWhatToDo(data.GetTable());
+
+        }
+    }
+}
