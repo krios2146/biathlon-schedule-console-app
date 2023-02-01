fe783896c6f13a092718c7f92e3f84e062f0a650
Vladimir Kidyaev <dreik2146@gmail.com>
2023-02-01 20:01:29 +0700

    chore: rename `Program` to `EntryPoint`

---
+++ EntryPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BiathlonProject
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            ProgrammLogic programm = new ProgrammLogic();
+            programm.InitiateProgramm();
+        }
+    }
+}
