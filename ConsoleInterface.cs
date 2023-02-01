e97e017f480c58bec5ec78291bd7fb6d0b2888e0
Vladimir Kidyaev <dreik2146@gmail.com>
2023-02-01 20:02:11 +0700

    feat: add new class `ConsoleInterface`

---
+++ ConsoleInterface.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiathlonProject
+{
+    class ConsoleInterface
+    {
+        Printer printer = new Printer();
+
+        public string AskUserWhatToDo(string[][] table)
+        {
+            printer.PrintTable(table);
+
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("1. Add new event");
+            Console.WriteLine("2. Change event data");
+            Console.WriteLine("3. Delete event");
+
+            string answer = Console.ReadLine();
+
+            if (!answer.Contains("1") || !answer.Contains("2") || !answer.Contains("3"))
+            {
+                Console.WriteLine("You should pick one of the options and enter it number");
+                AskUserWhatToDo(table);
+            }
+
+            return answer;
+        }
+
+    }
+}
