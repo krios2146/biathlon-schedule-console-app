ebbb6ab3eb10b9380bad5509d13b450705f7fca0
Vladimir Kidyaev <dreik2146@gmail.com>
2023-02-03 19:27:48 +0700

    fix: unit tests for `ConsoleLogic`

--- BiathlonProjectTests/ConsoleInterfaceTests.cs
+++ BiathlonProjectTests/ConsoleInterfaceTests.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BiathlonProject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BiathlonProjectTests
+namespace BiathlonProject.Tests
 {
-    internal class ConsoleInterfaceTests
+    [TestClass()]
+    public class ConsoleInterfaceTests
     {
+        [TestMethod()]
+        public void AskUserWhatToDoTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AskUserForNewEntryTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AskUserToUpdateEntryTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AskUserWhatEntryToDeleteTest()
+        {
+            Assert.Fail();
+        }
     }
 }
--- BiathlonProjectTests/ConsoleLogicTests.cs
+++ BiathlonProjectTests/ConsoleLogicTests.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BiathlonProjectTests1
+namespace BiathlonProjectTests
 {
     internal class ConsoleLogicTests
     {
