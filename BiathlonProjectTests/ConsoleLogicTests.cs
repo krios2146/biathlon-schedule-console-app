defb18a918bf09a381ede08f830310cade286bee
Vladimir Kidyaev <krios2146@gmail.com>
2023-02-04 16:49:00 +0700

    fix: ConsoleLogicTests now has empty tests methods

--- BiathlonProjectTests/ConsoleLogicTests.cs
+++ BiathlonProjectTests/ConsoleLogicTests.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BiathlonProject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BiathlonProjectTests
+namespace BiathlonProject.Tests
 {
-    internal class ConsoleLogicTests
+    [TestClass()]
+    public class ConsoleLogicTests
     {
+        [TestMethod()]
+        public void InitiateProgrammTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateNewEntryTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateEntryTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteEntryTest()
+        {
+            Assert.Fail();
+        }
     }
 }
